2c1a336da524adb76f654244f6d117a4f0dc6383
Liam Norman <liam.norman@zando.co.za>
2017-06-13 14:15:46 +0000

    Apply fixes from StyleCI

--- src/LoginCommand.php
+++ src/LoginCommand.php
@@ -2,12 +2,11 @@
 
 namespace YoutrackApiClientCli;
 
+use GuzzleHttp\Client;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use GuzzleHttp\Client;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Input\InputOption;
 
 class LoginCommand extends Command
 {
@@ -17,12 +16,11 @@ class LoginCommand extends Command
              ->setDescription('Login to Youtrack')
              ->addArgument('name', InputArgument::REQUIRED, 'Your username.')
             ->addArgument('password', InputArgument::REQUIRED, 'Your password.');
-
     }
 
     public function execute(InputInterface $input, OutputInterface $output)
     {
-        $url = "https://todotick.myjetbrains.com";
+        $url = 'https://todotick.myjetbrains.com';
 
         $username = $input->getArgument('name');
         $password = $input->getArgument('password');
@@ -30,17 +28,14 @@ class LoginCommand extends Command
         $client = new Client();
 
         $response = $client->post("$url/rest/user/login", [
-            'headers' => ['Content-Type' => 'application/x-www-form-urlencoded'],
+            'headers'     => ['Content-Type' => 'application/x-www-form-urlencoded'],
             'form_params' => ['login' => $username,
-            'password' => $password]
+            'password'                => $password, ],
         ]);
 
         $cookie = $response->getHeader('Set-Cookie');
 
-
         $message = sprintf('%s, %s', $response->getStatusCode(), $input->getArgument('name'));
         $output->writeln("<info>$message</info>");
     }
-
 }
-
